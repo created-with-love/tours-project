bc35dcba635c06a51e277623e569ca6f8bdc8ea1
Vladymyr Ivanchenko <vladymyr_ivanchenko@bigmir.net>
2020-12-27 13:49:42 +0200

    Добавляю готовый проект

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project';
 
 function App() {
-  return <div className="App"></div>;
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  return loading ? (
+    <main>
+      <Loading />
+    </main>
+  ) : (
+    <main>
+      {tours.length === 0 && (
+        <div className="title">
+          <h2>no tours left</h2>
+          <button type="button" onClick={fetchTours} className="btn">
+            Refresh
+          </button>
+        </div>
+      )}
+
+      {tours.length !== 0 && <Tours data={tours} removeTour={removeTour} />}
+    </main>
+  );
 }
 
 export default App;
